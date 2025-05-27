8e6238108a669b665114967602de9014cb21fa67
qyssss <26544143@qq.com>
2025-05-27 21:44:04 +0800

    mid

--- src/types/shims-vue3-markdown-it.d.ts
+++ src/types/shims-vue3-markdown-it.d.ts
@@ -1,1 +1,6 @@
 declare module 'vue3-markdown-it';
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
